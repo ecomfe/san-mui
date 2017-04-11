31975139ca2b734873f0bce02e46b55908a3757e
zhangzhiqiang37 <zhangzhiqiang04@baidu.com>
2017-04-11 13:11:20 +0800

    popover、pagination example升级

--- example/index.js
+++ example/index.js
@@ -9,6 +9,8 @@ import Button from './Button.san';
 import TextField from './TextField.san';
 import Menu from './Menu.san';
 import Tabs from './Tabs.san';
+import Pagination from './Pagination.san';
+import Popover from './Popover.san';
 
 import './index.styl';
 
@@ -18,5 +20,7 @@ router.add({rule: '/textfield', Component: TextField, target: '#root'});
 router.add({rule: '/textfield', Component: TextField, target: '#root'});
 router.add({rule: '/menu', Component: Menu, target: '#root'});
 router.add({rule: '/tabs', Component: Tabs, target: '#root'});
+router.add({rule: '/pagination', Component: Pagination, target: '#root'});
+router.add({rule: '/popover', Component: Popover, target: '#root'});
 
 router.start();
