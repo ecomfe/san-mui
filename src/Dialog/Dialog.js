43e4f256418e07faefc0239662750e6eb097c0d6
jinzhubaofu <leonlu@outlook.com>
2017-04-15 14:42:53 +0800

    优化 Dialog，添加 useMask 和 closeOnMaskClick 参数

--- src/Dialog/Dialog.js
+++ src/Dialog/Dialog.js
@@ -6,13 +6,16 @@
 import {create} from '../common/util/cx';
 import Layer from '../Layer/Layer';
 import * as Mask from '../Mask';
+import css from '../common/util/css';
 
 const cx = create('dialog');
 
 export default class Dialog extends Layer {
 
     static template = `
-        <div class="{{computedClassName}}" style="display: {{open ? 'block' : 'none'}}">
+        <div
+            class="{{computedClassName}}"
+            style="{{style}}">
             <h3 class="${cx.getPartClassName('title')}">
                 <slot name="title">{{title}}</slot>
             </h3>
@@ -25,22 +28,43 @@ export default class Dialog extends Layer {
         </div>
     `;
 
+    static computed = {
+        style() {
+            return css({
+                display: this.data.get('open') ? 'block' : 'none',
+                width: `${this.data.get('width')}px`
+            });
+        },
+
+        computedClassName() {
+            return cx(this).build();
+        }
+
+    };
+
     initData() {
         return {
             open: false,
-            computedClassName: cx(this).build(),
-            useMask: true
+            useMask: true,
+            closeOnClickMask: true,
+            width: 760
         };
     }
 
     attached() {
         super.attached();
+        let onMaskClick = this.onMaskClick = this.onMaskClick.bind(this);
         this.watch('open', open => {
-            console.log(open);
             if (this.data.get('useMask')) {
-                open ? Mask.show() : Mask.hide();
+                open ? Mask.show(onMaskClick) : Mask.hide();
             }
         });
+    }
+
+    onMaskClick() {
+        if (this.data.get('closeOnClickMask')) {
+            this.data.set('open', false);
+        }
     }
 
 
