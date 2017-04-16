efdf97eac0cd15b7be9dc019871ed286ee1533e4
menglingjun <menglingjun@baidu.com>
2017-04-17 02:08:34 +0800

    adapt button / a

--- src/Button/IconButton.js
+++ src/Button/IconButton.js
@@ -3,20 +3,20 @@
  * @author leon <ludafa@outlook.com>
  */
 
-import {Component} from 'san';
+import BaseButton from './Base';
 import {create} from '../common/util/cx';
 import Icon from '../Icon';
 import {CenterRipple} from '../Ripple';
 
 const cx = create('button');
 
-export default class IconButton extends Component {
+export default class IconButton extends BaseButton {
 
     static template = `
         <button
             class="{{computedClassName}}"
             disabled="{{disabled}}">
-            <san-icon><slot /></san-icon>
+            <san-icon icon="{{icon}}"><slot /></san-icon>
             <san-center-ripple />
         </button>
     `;
