d835d9f8aeebbc6a064f58b172e4c3e9dd3bb641
menglingjun <menglingjun@baidu.com>
2017-04-16 18:25:28 +0800

    index update

--- example/index.js
+++ example/index.js
@@ -3,43 +3,9 @@
  * @author ielgnaw(wuji0223@gmail.com)
  */
 
-import {router} from 'san-router';
-import Main from './Main';
-import Button from './Button.san';
-import TextField from './TextField.san';
-import Menu from './Menu.san';
-import Tabs from './Tabs.san';
-import Pagination from './Pagination.san';
-import Popover from './Popover.san';
-import Drawer from './Drawer.san';
-import Dialog from './Dialog.san';
-import Progress from './Progress.san';
-import DatePicker from './DatePicker.san';
-import Table from './Table.san';
-import Ripple from './Ripple.san';
-
 import './index.styl';
 
-let routes = {
-    '/': Main,
-    '/button': Button,
-    '/textfield': TextField,
-    '/menu': Menu,
-    '/tabs': Tabs,
-    '/pagination': Pagination,
-    '/popover': Popover,
-    '/drawer': Drawer,
-    '/progress': Progress,
-    '/datepicker': DatePicker,
-    '/dialog': Dialog,
-    '/table': Table,
-    '/ripple': Ripple
-};
+import App from './App';
 
-Object.keys(routes).forEach(rule => router.add({
-    rule,
-    Component: routes[rule],
-    target: '#root'
-}));
+new App().attach(document.querySelector('#root'))
 
-router.start();
