c611d313e9be84e370c7e2292362a0730b1f5c37
wuhy <sparklewhy@gmail.com>
2017-04-17 11:35:31 +0800

    up babel conf

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * @file export components
+ * @author ielgnaw(wuji0223@gmail.com)
+ */
+
+// import NotRecommended from './NotRecommended';
+// import Recommended from './Recommended';
+
+// export default {
+//     NotRecommended,
+//     Recommended
+// };
