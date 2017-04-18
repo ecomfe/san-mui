ac51b453de584a55c5e134038ba8d8f9141d9e74
erik <erik168@163.com>
2017-04-18 23:31:45 +0800

    mv tpl 2 js, and update demo

--- src/Menu/MenuItem.js
+++ src/Menu/MenuItem.js
@@ -6,7 +6,6 @@
 import san from 'san';
 import './MenuItem.styl';
 import padStyles from '../filters/padStyles';
-import template from './MenuItem.tpl';
 import Icon from '../Icon';
 import {TouchRipple} from '../Ripple';
 
@@ -15,7 +14,22 @@ function getSelectedText(title, label) {
 }
 
 export default san.defineComponent({
-    template,
+    template: `
+<div
+    on-click="select($event)"
+    class="sm-menuitem
+            {{ selected | yesToBe(' selected') }}
+            {{ leftIcon | yesToBe('has-left') }}"
+    value="{{value}}"
+>
+    <div class="sm-menuitem-left-icon" san-if="{{ leftIcon }}"><san-icon>{{ leftIcon }}</san-icon></div>
+    <p>{{ title }}</p>
+    <div class="sm-menuitem-right-icon-group">
+        <san-icon san-for="ri in rightIcons">{{ ri }}</san-icon>
+    </div>
+    <san-touch-ripple />
+</div>
+    `,
 
     components: {
         'san-icon': Icon,
