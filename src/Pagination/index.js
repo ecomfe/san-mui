3d6e914479b6cd7ec251118360fa89cb360d30fe
zhangzhiqiang37 <zhangzhiqiang04@baidu.com>
2017-04-24 14:50:12 +0800

    pagination bugifx

--- src/Pagination/index.js
+++ src/Pagination/index.js
@@ -32,17 +32,17 @@ export default san.defineComponent({
 
                     <span san-if="current >= pageGroupLen" class="pre-group" on-click="setCurrentPage(current - pageGroupLen, 1)">...</span>
 
-                    <span class="page-num" san-if="current - 2 >= 2 && current - 2 < totalPage" on-click="setCurrentPage(current - 2)" da-index="aaa">{{ current - 2 }}</span>
-                    <span class="page-num" san-if="current - 1 >= 2 && current - 1 < totalPage" on-click="setCurrentPage(current - 1)" da-index="bbb">{{ current - 1 }}</span>
-                    <span class="page-num current" san-if="current >= 2 && current < totalPage" on-click="setCurrentPage(current)" da-index="ccc">{{ current }}</span>
-                    <span class="page-num" san-if="current + 1 >= 2 && current + 1 < totalPage" on-click="setCurrentPage(current + 1)" da-index="ddd">{{ current + 1 }}</span>
-                    <span class="page-num" san-if="current + 2 >= 3 && current + 2 < totalPage" on-click="setCurrentPage(current + 2)" da-index="eee">{{ current + 2 }}</span>
+                    <span class="page-num" san-if="current - 2 >= 2 && current - 2 < totalPage" on-click="setCurrentPage(current - 2)">{{ current - 2 }}</span>
+                    <span class="page-num" san-if="current - 1 >= 2 && current - 1 < totalPage" on-click="setCurrentPage(current - 1)">{{ current - 1 }}</span>
+                    <span class="page-num current" san-if="current >= 2 && current < totalPage" on-click="setCurrentPage(current)">{{ current }}</span>
+                    <span class="page-num" san-if="current + 1 >= 2 && current + 1 < totalPage" on-click="setCurrentPage(current + 1)">{{ current + 1 }}</span>
+                    <span class="page-num" san-if="current + 2 >= 3 && current + 2 < totalPage" on-click="setCurrentPage(current + 2)">{{ current + 2 }}</span>
 
                     <span san-if="totalPage - current - 1 >= pageGroupLen" class="next-group" on-click="setCurrentPage(current + pageGroupLen, 1)">...</span>
 
                     <span san-if="totalPage > 1" class="page-num last-page{{ current === totalPage ? ' current' : '' }}" on-click="setCurrentPage(totalPage)">{{ totalPage }}</span>
 
-                    <span class="next-page{{ current === totalPage : ' disable' : '' }}" on-click="setCurrentPage(current + 1)">
+                    <span class="next-page{{ current === totalPage ? ' disable' : '' }}" on-click="setCurrentPage(current + 1)">
                         <label san-if="nextPageText">{{ nextPageText }}</label>
                         <label san-else class="next-page-icon arrow-icon"></label>
                     </span>
