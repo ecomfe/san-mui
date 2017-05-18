a3b47df1e4872c27a8713f56f7a811257fbf0fb7
nemo <luoxincheng@baidu.com>
2017-05-18 13:40:30 +0800

    no message

--- src/Uploader/Uploader.js
+++ src/Uploader/Uploader.js
@@ -7,15 +7,28 @@ import san from 'san';
 
 export default san.defineComponent({
     template: `
-        <div class="upload"></div>
+        <div class="upload sm-button variant-info variant-raised">
+        	上传
+        	<input type="file" on-change="reciveFile($event)"/>
+        </div>
     `,
     components: {
         
     },
-    initData() {
-        return {
-            
-        };
+    inited() {
+    	console.log(this.data.get('opt'))
+    },
+    reciveFile(event) {
+    	var opt = this.data.get('opt')
+    	var xhr = new XMLHttpRequest()
+    	var formData = new FormData();
+    	formData.append('file', event.target.files[0])
+    	xhr.onreadystatechange = function(state) {
+    		console.log(state)
+    	}
+    	xhr.open('POST', opt.action)
+    	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.send(formData);
     }
 });
 
