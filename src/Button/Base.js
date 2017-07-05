6f1c48c28d304885b9970af5178657da8a46cd4a
menglingjun <menglingjun@baidu.com>
2017-07-05 19:01:59 +0800

    fix button -> a

--- src/Button/Base.js
+++ src/Button/Base.js
@@ -3,31 +3,63 @@
  * @author junmer
  */
 
-import {Component} from 'san';
+import {Component, ExprType} from 'san';
 
+/**
+ * 生成属性
+ *
+ * @param  {Element} element Component
+ * @param  {string}  name    属性名
+ */
 function genProp(element, name) {
+
     element.aNode.props.push({
         name: name,
         expr: {
-            type: 3,
-            paths: [{
-                type: 1,
-                value: name
-            }]
+            type: ExprType.TEXT,
+            segs: [{
+                type: ExprType.INTERP,
+                expr: {
+                    type: ExprType.ACCESSOR,
+                    paths: [{
+                        type: ExprType.STRING,
+                        value: name
+                    }]
+                },
+                filters: []
+            }],
+            raw: `\{\{${name}\}\}`
         },
         raw: `\{\{${name}\}\}`
     });
+
 }
 
+/**
+ * 是否有数据
+ *
+ * @param  {Element} element Component
+ * @param  {string}  name    属性名
+ * @return {boolean}         结构
+ */
+function hasData(element, name) {
+    return name in element.data.raw;
+}
+
+/**
+ * 适配 链接标签
+ *
+ * @param  {Element} element Component
+ */
 function adaptAnchor(element) {
 
-    if (element.data.get('href')) {
+    if (hasData(element, 'href')) {
         element.aNode.tagName = 'A';
         element.tagName = 'a';
         genProp(element, 'href');
     }
 
-    if (element.data.get('target')) {
+    if (hasData(element, 'target')) {
         genProp(element, 'target');
     }
 
